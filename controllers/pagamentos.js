af52a9be9768147a427b527b023310e7bd8b2725
Bruno Cesar Aparecido Gaudino <brunogaudino@brq.com>
2017-08-04 16:04:52 -0300

    Aula 3, rota para confirmar pagamentos, cancelar pagamentos

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,9 +1,86 @@
+/*
+Existem diversos status code HTTP , conhecê-los e utilizá-los corretamente é extremamente importante para o bom desenho de uma api REST. Veja abaixo alguns dos principais códigos e seus significados:
+100 Continue: o servidor recebeu a solicitação e o cliente pode continuar a comunicação.
+200 Ok: tudo ocorreu como esperado.
+201 Created: um novo recurso foi criado no servidor.
+301 Moved: a url solicitada foi movida.
+400 Bad Request: problemas na requisição do cliente.
+404 Not Found: a url solicitada não foi encontrada.
+500 Internal Server Error: algo inesperado aconteceu do lado do servidor
+Status code link:
+https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
+-------------------------------------------------------------------------------------------------------------------
+REQUISIÇÕES DAS APIs
+Requisições post
+curl http://localhost:3001/pagamentos/pagamento -X POST -v -H "Content-type: application/json" -d @files/pagamento.json; echo
+curl http://localhost:3001/pagamentos/pagamento -X POST -v -H "Content-type: application/json" -d @files/pagamento.json | json_pp
+
+Requisições put
+curl -X PUT http://localhost:3001/pagamentos/pagamento/10 -v
+
+Requisições delete
+curl -X DELETE http://localhost:3001/pagamentos/pagamento/10 -v
+*/
+
 module.exports = function(app){
   app.get('/pagamentos', function(req, res){
     console.log("Recebida a requisição de teste");
     res.send('Ok');
   });
 
+  app.delete('/pagamentos/pagamento/:id', function(req, res){
+
+    var pagamento = {};
+    var id = req.params.id;
+
+    pagamento.id = id;
+    pagamento.status = 'CANCELADO';
+
+    var connection = app.persistencia.connectionFactory();
+    var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+    pagamentoDao.atualiza(pagamento, function(erro){
+
+      if(erro){
+        res.status(500).send(erro);
+        return;
+      }
+      console.log("pagamento cancelado");
+      res.status(204).send(pagamento);
+
+    });
+
+  });
+
+  //Requisição que atualiza o pagamento no banco
+  app.put('/pagamentos/pagamento/:id', function(req, res){
+
+    var pagamento = {};
+    var id = req.params.id;
+
+    pagamento.id = id;
+    pagamento.status = 'CONFIRMADO';
+
+    var connection = app.persistencia.connectionFactory();
+    var pagamentoDao = new app.persistencia.PagamentoDao(connection);
+
+    pagamentoDao.atualiza(pagamento, function(erro){
+
+      if(erro){
+        res.status(500).send(erro);
+        return;
+      }
+
+      console.log("pagamento criado");
+      res.send(pagamento);
+
+    });
+
+
+  });
+
+
+  //Faz o post de pagamento no db
   app.post('/pagamentos/pagamento', function(req,res){
 
     req.assert("forma_de_pagamento", "Forma de pagamento é obrigatória.").notEmpty();
@@ -42,20 +119,3 @@ module.exports = function(app){
   });
 
 }
-
-/*
-REQUISIÇÕES DAS APIs
-
-curl http://localhost:3001/pagamentos/pagamento -X POST -v -H "Content-type: application/json" -d @files/pagamento.json; echo
-
-curl http://localhost:3001/pagamentos/pagamento -X POST -v -H "Content-type: application/json" -d @files/pagamento.json | json_pp
-
-Existem diversos status code HTTP , conhecê-los e utilizá-los corretamente é extremamente importante para o bom desenho de uma api REST. Veja abaixo alguns dos principais códigos e seus significados:
-100 Continue: o servidor recebeu a solicitação e o cliente pode continuar a comunicação.
-200 Ok: tudo ocorreu como esperado.
-201 Created: um novo recurso foi criado no servidor.
-301 Moved: a url solicitada foi movida.
-400 Bad Request: problemas na requisição do cliente.
-404 Not Found: a url solicitada não foi encontrada.
-500 Internal Server Error: algo inesperado aconteceu do lado do servidor
-*/
