86afb0b340282af4374eb03de72def1270dc8a08
Bruno Gaudino <brunocesaragweb@gmail.com>
2017-08-21 22:32:18 -0300

    Trabalhando com logs na aplicação, instalando o winston

---
+++ payfast/servicos/logger.js
@@ -0,0 +1,17 @@
+var winston = require('winston');
+
+var logger = new winston.Logger({
+  transports: [
+    new winston.transports.File({
+      level: "info",
+      filename: "logs/payfast.log",
+      maxsize: 100000,
+      maxFiles: 10,
+      colorize: false
+    })
+  ]
+});
+
+logger.log('Log utilizando o winston');
+logger.log('info','Log utilizando winston e info');
+logger.info('Log mais maroto');
